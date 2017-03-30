a5291f0cb7e2d5b810304a407c252796825b8928
Chickenmcheese <thewalkingdoge@protonmail.com>
2017-03-30 20:35:06 +0100

    Add files via upload

--- asd2.c
+++ asd2.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[]) {
 		printf("no args given\n");
 		exit(1);
 	}
-	printf("Argument given: %s\n", argv[1]);
+	//printf("Argument given: %s\n", argv[1]);
 	
 	fgets (array, BUFFERSIZE, stdin);
 
