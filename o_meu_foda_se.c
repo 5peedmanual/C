e64205932f4da7609267844e44fd91d65b62c8bf
Chickenmcheese <thewalkingdoge@protonmail.com>
2017-03-31 22:06:58 +0100

    Add files via upload

--- o_meu_foda_se.c
+++ o_meu_foda_se.c
@@ -4,13 +4,12 @@
 #include <ctype.h>
 #include <math.h>
 
-void escreve_IEEE_strings(char sinal, char * expoente, char* mantissa, float valor) {
-    printf ("   sinal: %c\n", sinal);
-    printf ("expoente: %s\n", expoente);
-    printf ("mantissa: %s\n", mantissa);
-    printf ("   valor: %.10f\n", valor);
+void escreve_IEEE_valores(char sinal, int expoente, float mantissa,
+float valor) {
+	printf("%c %.10f * 2^%d\n", sinal, mantissa, expoente);
+	printf("valor: %.10f\n", valor);
 }
-
+//binary para o expoente
 int convertBinaryToDecimal(long long n) {
     int decimalNumber = 0, i = 0, remainder;
     while (n!=0) {
@@ -21,47 +20,55 @@ int convertBinaryToDecimal(long long n) {
     }
     return decimalNumber;
 }
-
-int expo(char *expoente) {
+//expoente string to int
+int function_expoente(char *expoente) {
 	int e = atoi(expoente);
 	int e_int = convertBinaryToDecimal(e);
 	return e_int -127;
 	
 }
-float man(char *mantissa) {
+//mantissa
+float function_mantissa(char *mantissa) {
 	int i;
-	float f = 1;
+	float num = 1;
   	for (i = 0; i < strlen (mantissa); i++) {
       		if (mantissa[i] == '1')
-		f += (float) 1 *(pow (2, -(i + 1)));
+		num += (float) 1 *(pow (2, -(num + 1)));
     	}
-  	return f;
+  	return num;
 }
 
-
-float final(char sinal, float e, float m) {
-	float v;
+//final
+float function_final(char sinal, float final_expoente, float final_mantissa) {
+	float num;
 	if ( sinal == '1') {
-		v = -1 * (m) * pow(2,e);
+		num = -1 * (final_mantissa) * pow(2,final_expoente);
 	} else {
-		v = 1 * (m) * pow(2,e);
+		num = 1 * (final_mantissa) * pow(2,final_expoente);
 	}
-	return v;
+	return num;
 }
 
-int main(int argc, char *argv[]) {	
+int main(int argc, char *argv[]) {
+
 	int i;
-	char binary[33], sinal, expoente[8], mantissa[23];
+	char binary[33], sinal, sinal_print, expoente[8], mantissa[23];
 	char *pointer_to_binary, *sinal_pointer, *expoente_start_pointer, *expoente_pointer, *mantissa_start_pointer;
 	float valor;
 	long long n;
 	pointer_to_binary = binary;
 
 	fgets(binary, 33, stdin);
-	//scanf("%s", &binary);
-	//strcpy(binary, "01000001001011000000000000000000\0");
 	sinal_pointer = pointer_to_binary;
+	//sinal for the functions
 	sinal = binary[0];
+	//print_sinal
+	if ( binary[0] == '1' ) {
+		sinal_print = '-';
+	} else {
+		sinal_print = '+';
+	}
+
 	expoente_start_pointer = pointer_to_binary + 1; //start
 
 	for ( i = 0; i < 9; i++) { 
@@ -75,16 +82,19 @@ int main(int argc, char *argv[]) {
 	mantissa_start_pointer = pointer_to_binary + 9;
 	for ( i = 0; i < 24; i++ ) {
 		if ( i == 23) {
-			mantissa[i] = '\0';
+			mantissa[i] = '\0'; //Add the null pointer to the end
 			break;
 		}
 		mantissa[i] = *mantissa_start_pointer;
 		mantissa_start_pointer += 1;
 	}
-	int e = expo(expoente);
-	float m = man(mantissa);
-	valor = final(sinal, e, m);
-	escreve_IEEE_strings(sinal, expoente, mantissa, valor);
+
+	//function calls
+	int final_expoente = function_expoente(expoente);
+	float final_mantissa = function_mantissa(mantissa);
+	valor = function_final(sinal, final_expoente, final_mantissa);
+	escreve_IEEE_valores(sinal_print, final_expoente, final_mantissa, valor);
+
 	return 0;
 }
 
